456a50f37d8726f61ed4c562f92d8cb3fe9b9882
TrystanScottLambert <TrystanScottLambert@gmail.com>
2022-11-16 17:46:15 +0200

    simple straight line fitting added.

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 
 import pylab as plt
 import numpy as np
+from scipy.optimize import curve_fit
 #from decam import DecamImage
 from sex_catalog import SExtractorCat
+
+
+def straight_line(x, c):
+    return c
 
 #INFILE = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/i/c4d_211021_003940_osj_i_vik1_skysubtracted_flux.fits.fz'
 INFILE  = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/i/test.cat'
@@ -23,14 +28,32 @@ converted_dec_mags = pan_mags - 0.155 * (pan_cat['rMeanPSFMag'] - pan_mags) + 0.
 converted_dec_mags_error = np.hypot(pan_mags_uncertainty, 0.155*np.hypot(pan_mags_uncertainty, pan_cat['rMeanPSFMagErr']))
 cut = np.where(converted_dec_mags < 30)[0]
 
+difference = np.array(list(decam_mags)) - np.array(list(converted_dec_mags))
+difference_errors = np.hypot(np.array(list(converted_dec_mags_error)), np.array(list(decam_mags_uncertainty)))
+
 plt.errorbar(decam_mags, pan_mags, xerr=decam_mags_uncertainty,
-             yerr=pan_mags_uncertainty, fmt='ro', ms=3, capsize=5)
+             yerr=pan_mags_uncertainty, fmt='ro', ms=3, capsize=5,ecolor='k')
 plt.xlabel('decam_mags')
 plt.ylabel('pan_stars_mags')
 plt.show()
 
 plt.errorbar(np.array(decam_mags)[cut], np.array(converted_dec_mags)[cut], xerr=np.array(decam_mags_uncertainty)[cut], 
-             yerr=np.array(converted_dec_mags_error)[cut], fmt='ro', ms=3, capsize=5)
+             yerr=np.array(converted_dec_mags_error)[cut], fmt='ro', ms=3, capsize=5, ecolor='k')
 plt.xlabel('decam_mags')
 plt.ylabel('pan_stars_mags converted into decam Mags')
+plt.show()
+
+x_data = np.array(decam_mags)[cut]
+y_data = np.array(difference)[cut]
+delta_y = np.array(difference_errors)[cut]
+delta_x = np.array(decam_mags_uncertainty)[cut]
+cut = np.where((x_data <= -12) & (x_data >= -14))[0]
+a_fit, cov=curve_fit(straight_line, x_data[cut], y_data[cut],sigma=delta_y[cut],absolute_sigma=True)
+uncertainties = np.sqrt(np.diag(cov))
+c = a_fit[0]
+plt.errorbar(x_data, y_data, xerr=delta_x,
+             yerr=delta_y, fmt='ro', ms=3, capsize=5, ecolor='k')
+plt.xlabel('decam_mags')
+plt.ylabel('pan_stars_mags converted into decam Mags - decam mags')
+plt.axhline(c,lw=4,color='k', ls='--')
 plt.show()
