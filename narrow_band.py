15c20ed9280de2c2a012d225d9116a9b14ca77c1
TrystanScottLambert <TrystanScottLambert@gmail.com>
2022-11-25 16:55:31 +0200

    Trying my best here.

--- narrow_band.py
+++ narrow_band.py
@@ -1,9 +1,12 @@
 """Narrow band analysis and fitting."""
 
 import pylab as plt
-import pandas as pd
 from sex_catalog import SExtractorCat
 
+def n_964(filters, alpha, zpt):
+    """convert n_964"""
+    z_filter, y_filter = filters
+    return z_filter - alpha*(z_filter - y_filter) - zpt
 
 if __name__ == '__main__':
     INFILE_SEX = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/N964/test.cat'
@@ -15,7 +18,7 @@ if __name__ == '__main__':
     decam_catalog_y, pan_cat_y = decam_all.cross_match_with_panstars(INFILE_Y)
     #decam_catalog_final = pd.merge(decam_catalog_y, decam_catalog_z)
     #panstars_catalog_final = pd.merge(pan_cat_y, pan_cat_z)
-    y = decam_catalog_z['MAG_BEST'].values# - pan_cat_z['zMeanPSFMag'].values
-    x = pan_cat_z['zMeanPSFMag'].values - pan_cat_z['yMeanPSFMag'].values
-    plt.scatter(x, y)
+    delta_pan = pan_cat_z['zMeanPSFMag'].values - pan_cat_z['yMeanPSFMag'].values
+    delta_decam = decam_catalog_z['MAG_BEST'].values - pan_cat_z['zMeanPSFMag'].values
+    plt.scatter(delta_pan, delta_decam)
     plt.show()
