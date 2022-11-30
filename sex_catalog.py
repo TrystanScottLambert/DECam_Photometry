8cc82ac33b4195e68cc5479b0eebf37ad13e7219
TrystanScottLambert <TrystanScottLambert@gmail.com>
2022-11-30 16:57:29 +0200

    Added to_region for Sextractor objects.

--- sex_catalog.py
+++ sex_catalog.py
@@ -3,8 +3,11 @@
 import numpy as np
 import pylab as plt
 import pandas as pd
+from astropy.io import fits
+from reproject.mosaicking import find_optimal_celestial_wcs
 from find_stars import cross_match
 import sex_utils as su
+from decam import write_positions_as_region_file
 
 class SExtractorCat:
     """Main class for sextractor catalogs."""
@@ -33,3 +36,20 @@ class SExtractorCat:
         dec_decam = np.array(list(self.catalog['DELTAPEAK_J2000']))
         plt.scatter(ra_decam, dec_decam)
         plt.show()
+
+    def to_region_file(self, decam_file: str, outfile: str, color: str = 'orange') -> None:
+        """Converts the points into a region file."""
+        hdu = fits.open(decam_file)
+        wcs, _ = find_optimal_celestial_wcs(hdu[1:])
+
+        ra_positions = self.catalog['ALPHAPEAK_J2000'].values
+        dec_positions = self.catalog['DELTAPEAK_J2000'].values
+        x_positions, y_positions = wcs.world_to_pixel_values(ra_positions, dec_positions)
+        write_positions_as_region_file(np.transpose((x_positions, y_positions)), outfile, color)
+
+if __name__ == '__main__':
+    DECAM_FITS_FILE = \
+        '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/i/c4d_211021_003940_osj_i_vik1.fits.fz'
+    SEX_FILE = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/i/test.cat'
+    i_cat = SExtractorCat(SEX_FILE)
+    i_cat.to_region_file(DECAM_FITS_FILE, 'delete_region_file.reg', 'red')
