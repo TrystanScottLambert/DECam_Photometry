b55c3a166b95502838d9257320d6ece1d3461f26
TrystanScottLambert <TrystanScottLambert@gmail.com>
2022-12-04 18:36:54 +0200

    Added removing based on exptime.

--- narrow_band.py
+++ narrow_band.py
@@ -93,7 +93,8 @@ if __name__ == '__main__':
     N964_DIR = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/N964/'
     N964_SEX_CAT = N964_DIR + 'test.cat'
     N964_DECAM_CAT = N964_DIR + 'c4d_210831_050404_osj_N964_vik1.fits.fz'
-
+    N964_EXP_MAP = N964_DIR + 'c4d_210831_050404_ose_N964_vik1.fits'
     #fwhm_plot(N964_SEX_CAT)
     n964_sex_cat = SExtractorCat(N964_SEX_CAT)
+    test = n964_sex_cat.remove_sources_based_on_exposure_map(N964_EXP_MAP)
     n964_sex_cat.to_region_file(N964_DECAM_CAT, N964_DIR + 'N964.reg', only_galaxies=True)
--- sex_catalog.py
+++ sex_catalog.py
@@ -4,7 +4,10 @@ import numpy as np
 import pylab as plt
 import pandas as pd
 from astropy.io import fits
+from astropy.wcs import WCS
 from reproject.mosaicking import find_optimal_celestial_wcs
+from reproject import reproject_interp
+from reproject.mosaicking import reproject_and_coadd
 from find_stars import cross_match
 import sex_utils as su
 from decam import write_positions_as_region_file
@@ -51,6 +54,24 @@ class SExtractorCat:
             x_positions = x_positions[cut]
             y_positions = y_positions[cut]
         write_positions_as_region_file(np.transpose((x_positions, y_positions)), outfile, color)
+
+    def remove_sources_based_on_exposure_map(self, exp_map: str) -> None:
+        """Uses an expsore map to exlude sources with no exposure times."""
+        hdul = fits.open(exp_map)
+        exptime = np.zeros(len(self.catalog['ALPHAPEAK_J2000']))
+        for hdu in hdul[1:]:
+            wcs = WCS(hdu.header)
+            x_pixels, y_pixels = wcs.world_to_pixel_values(
+                self.catalog['ALPHAPEAK_J2000'], self.catalog['DELTAPEAK_J2000'])
+            x_pixels, y_pixels = x_pixels.astype(int), y_pixels.astype(int)
+            cut_y = np.where((y_pixels < hdu.shape[0]) & (y_pixels >= 0))[0]
+            cut_x = np.where((x_pixels < hdu.shape[0]) & (x_pixels >= 0))[0]
+            final_cut = np.intersect1d(cut_x, cut_y).astype(int)
+            vals = [hdu.data[y_pixels[index], x_pixels[index]] for index in final_cut]
+            exptime[final_cut] = vals
+        self.catalog['exptime'] = exptime
+        self.catalog = self.catalog[self.catalog.exptime != 0]
+
 
 if __name__ == '__main__':
     DECAM_FITS_FILE = \
