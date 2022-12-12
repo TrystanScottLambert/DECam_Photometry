068b3dd9b283658bc708ca3a9bb1c0b090504d17
TrystanScottLambert <TrystanScottLambert@gmail.com>
2022-12-12 16:46:54 +0200

    updated trim to included weights

--- trim.py
+++ trim.py
@@ -13,35 +13,41 @@ def read_in_fits(infile: str):
     wcs = WCS(hdu[0].header)
     return hdu, wcs
 
-def cutout_position(hdu, position: SkyCoord, size: float, wcs: WCS):
-    """Cutsout the position"""
-    cutout = Cutout2D(hdu[0].data, position, size, wcs=wcs)
-    return cutout
-
 def make_new_fits_file(hdu, cutout_object: Cutout2D, outfile: str):
     """Writes a new fits object based on the cutout data."""
     new_hdu = fits.PrimaryHDU()
     new_hdu.data = cutout_object.data
     new_hdu.header = hdu[0].header
-    new_hdu.header.update(cutout.wcs.to_header())
+    new_hdu.header.update(cutout_object.wcs.to_header())
     new_hdu.writeto(outfile, overwrite=True)
+
+def cut_fits_image(fits_image: str, position: SkyCoord, size: float, outfile: str) -> None:
+    """Cuts a fits image based on a central point in a box shape defined by size."""
+    hdu, wcs = read_in_fits(fits_image)
+    cutout = Cutout2D(hdu[0].data, position, size, wcs=wcs)
+    make_new_fits_file(hdu, cutout, outfile)
 
 
 if __name__ == '__main__':
     INFILE_I = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/i/i_band_coadd.fits'
+    INFILE_I_WEIGHT = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/i/i_band_weight_coadd.fits'
+
     INFILE_Z = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/z/z_band_coadd.fits'
+    INFILE_Z_WEIGHT = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/z/z_band_weight_coadd.fits'
+
     INFILE_N964 = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/N964/N964_band_coadd.fits'
+    INFILE_N964_WEIGHT = '/home/trystan/Desktop/Work/PhD/DECAM/correct_stacks/N964/N964_band_weight_coadd.fits'
     SIZE = 2*u.deg
 
     hdu, wcs = read_in_fits(INFILE_N964)
-    position = SkyCoord(hdu[0].header['CRVAL1']*u.deg, hdu[0].header['CRVAL2']*u.deg)
-    cutout = cutout_position(hdu, position, SIZE, wcs)
-    make_new_fits_file(hdu, cutout, 'test_delete_N964.fits')
+    n964_position = SkyCoord(hdu[0].header['CRVAL1']*u.deg, hdu[0].header['CRVAL2']*u.deg)
 
-    hdu_i, wcs_i = read_in_fits(INFILE_I)
-    cutout_i = cutout_position(hdu_i, position, SIZE, wcs_i)
-    make_new_fits_file(hdu_i, cutout_i, 'test_delete_i.fits')
+    cut_fits_image(INFILE_I, n964_position, SIZE, 'test_delete_i.fits')
+    cut_fits_image(INFILE_I_WEIGHT, n964_position, SIZE, 'test_delete_i_weight.fits')
 
-    hdu_z, wcs_z = read_in_fits(INFILE_Z)
-    cutout_z = cutout_position(hdu_z, position, SIZE, wcs_z)
-    make_new_fits_file(hdu_z, cutout_z, 'test_delete_z.fits')
+    cut_fits_image(INFILE_Z, n964_position, SIZE, 'test_delete_z.fits')
+    cut_fits_image(INFILE_Z_WEIGHT, n964_position, SIZE, 'test_delete_z_weight.fits')
+
+    cut_fits_image(INFILE_N964, n964_position, SIZE, 'test_delete_n964.fits')
+    cut_fits_image(INFILE_N964_WEIGHT, n964_position, SIZE, 'test_delete_n964_weight.fits')
+    
