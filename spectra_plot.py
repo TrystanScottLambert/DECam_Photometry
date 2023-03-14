1a60a184ddef4db315050dedb8c5adc025c01a1c
Trystan Lambert <trystanscottlambert@gmail.com>
2023-03-14 12:42:17 -0300

    Added i and z band to synphot analysis.

--- spectra_plot.py
+++ spectra_plot.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import astropy.units as u
-import pylab as plt
 from synphot import Observation, SpectralElement, SourceSpectrum
 from synphot.models import Empirical1D
 
@@ -10,12 +9,34 @@ from synphot.models import Empirical1D
 
 if __name__ == '__main__':
     SPECTRA_FILE = '../QSO_Spectra/J2348_dered.txt'
-    BANDPASS_FILE = '../QSO_Spectra/NB964_DECam_F29.txt'
-    band_pass = SpectralElement.from_file(BANDPASS_FILE, wave_unit='nm')
+    NARROW_BANDPASS_FILE = '../QSO_Spectra/NB964_DECam_F29.txt'
+    I_BANDPASS_FILE = '../QSO_Spectra/decam_i_bandpass.txt'
+    Z_BANDPASS_FILE = '../QSO_Spectra/decam_z_bandpass.txt'
+
+
+    #Define the filter objects
+    narrow_band_pass = SpectralElement.from_file(NARROW_BANDPASS_FILE, wave_unit='nm')
+    i_band_pass = SpectralElement.from_file(I_BANDPASS_FILE, wave_unit='nm')
+    z_band_pass = SpectralElement.from_file(Z_BANDPASS_FILE, wave_unit='nm')
+
+    #Define the source spectrum
     wave, flux, err = np.loadtxt(SPECTRA_FILE, unpack=True)
     wave = wave * u.angstrom
     flux = flux * (1e-17 * u.erg)/u.s/(u.cm**2)/u.angstrom
     spectrum = SourceSpectrum(Empirical1D, points=wave, lookup_table=flux)
 
-    observation = Observation(spectrum, band_pass)
-    print(observation.effstim(u.ABmag))
+
+    #Convolving the filters with the spectra
+    narrow_observation = Observation(spectrum, narrow_band_pass)
+    i_observation = Observation(spectrum, i_band_pass, force='taper')
+    z_observation = Observation(spectrum, z_band_pass, force = 'taper')
+
+    #Working out the AB magnitudes
+    narrow_mag = narrow_observation.effstim(u.ABmag)
+    i_mag = i_observation.effstim(u.ABmag)
+    z_mag = z_observation.effstim(u.ABmag)
+
+    #Working out the colors.
+    i_n = i_mag - narrow_mag
+    i_z = i_mag - z_mag
+    n_z = narrow_mag - z_mag
