b10d0691d601ebec035dd2e1f8a517e485fd61c7
Trystan Lambert <trystanscottlambert@gmail.com>
2023-03-17 09:56:01 -0300

    updated convert

--- convert_sexcat_into_region.py
+++ convert_sexcat_into_region.py
@@ -3,12 +3,11 @@
 import numpy as np
 import astropy.units as u
 from astropy.coordinates import SkyCoord
-from tqdm import tqdm
 
 def read_in_positions(file_name: str):
-    """Reads in the first and second column which are ra and dec respectively"""
-    ra, dec = np.loadtxt(file_name, usecols=(0,1), unpack=True)
-    return ra, dec
+    """Reads in the first and second column which are r_a and dec respectively"""
+    r_a, dec = np.loadtxt(file_name, usecols=(0,1), unpack=True)
+    return r_a, dec
 
 def replace_letter_with_colons(string: str):
     """30d45m64s --> 30:45:64"""
@@ -17,20 +16,20 @@ def replace_letter_with_colons(string: str):
         string = string.replace(letter, ':')
     return string.replace('s','')
 
-def convert_decimal_degrees_into_celestial(ra: float, dec: float):
+def convert_decimal_degrees_into_celestial(r_a: float, dec: float):
     """Takes floats and returns them in the hh:mm:ss and dd:mm:ss format."""
-    c = SkyCoord(ra = ra*u.deg, dec = dec*u.deg)
+    c = SkyCoord(r_a = r_a*u.deg, dec = dec*u.deg)
     return replace_letter_with_colons(c.to_string('hmsdms'))
 
 
 def convert_cat_to_reg(sextractor_catalog: str, outfile: str = 'sex_cat.reg'):
     """takes a .cat file and returns a .reg file"""
-    ra, dec = read_in_positions(sextractor_catalog)
-    positions = [convert_decimal_degrees_into_celestial(ra[i], dec[i]) for i in range(len(ra))]
-    f = open(outfile, 'w')
-    for pos in tqdm(positions):
-        f.write(f'circle {pos} 2.5" \n')
-    f.close()
+    r_a, dec = read_in_positions(sextractor_catalog)
+    positions = [convert_decimal_degrees_into_celestial(r_a[i], dec[i]) for i in range(len(r_a))]
+    file = open(outfile, 'w', encoding='utf8')
+    for pos in positions:
+        file.write(f'circle {pos} 2.5" \n')
+    file.close()
 
 
 if __name__ == '__main__':
