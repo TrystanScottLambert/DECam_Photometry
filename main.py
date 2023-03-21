50c60d2773890a50f2ee9bd1c21d3d294d3bc6d8
Trystan Lambert <trystanscottlambert@gmail.com>
2023-03-21 12:08:09 -0300

    making the code more readable

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ from decam import write_positions_as_region_file
 def remove_outliers(array, sigma):
     """returns a mask of values within the given sigma."""
     median, std = np.median(array), np.std(array)
-    good_idx = np.where((array >= median-sigma*std) &
-                        (array <= median + sigma*std))[0]
+    good_idx = np.where((array >= median-sigma*std) & (array <= median + sigma*std))[0]
     return good_idx
 
 
@@ -30,28 +29,22 @@ def straight_line(x_array, constant):
 def read_in_wide_band(sextractor_file_name: str, panstars_cat_name: str):
     """Reading in the catalogs."""
     decam_catalog = SExtractorCat(sextractor_file_name)
-    decam_catalog, pan_cat = decam_catalog.cross_match_with_panstars(
-        panstars_cat_name)
+    decam_catalog, pan_cat = decam_catalog.cross_match_with_panstars(panstars_cat_name)
     return decam_catalog, pan_cat
 
 
-def make_wide_band_mags(
-        decam_df: pd.DataFrame, panstars_cat: pd.DataFrame, band: str
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def make_wide_band_mags(decam_df: pd.DataFrame, panstars_cat: pd.DataFrame, band: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Gets the magnitudes and errors for a wide-band filter."""
 
-    dec_mags = decam_df['MAG_BEST'].values
-    dec_mags_uncertainty = decam_df['MAGERR_BEST'].values
+    dec_mags = decam_df['MAG_AUTO'].values
+    dec_mags_uncertainty = decam_df['MAGERR_AUTO'].values
     pan_mags = panstars_cat[f'{band}MeanPSFMag'].values
     pan_mags_uncertainty = panstars_cat[f'{band}MeanPSFMagErr'].values
 
     converted_mags, converted_mags_errors = CONVERT[band](panstars_cat)
+    mag_diff, mag_diff_err = calculate_difference_between_mags(dec_mags, dec_mags_uncertainty, converted_mags, converted_mags_errors)
 
-    mag_diff, mag_diff_err = calculate_difference_between_mags(
-        dec_mags, dec_mags_uncertainty, converted_mags, converted_mags_errors)
-
-    return dec_mags, dec_mags_uncertainty, pan_mags, pan_mags_uncertainty, \
-        converted_mags, converted_mags_errors, mag_diff, mag_diff_err
+    return dec_mags, dec_mags_uncertainty, pan_mags, pan_mags_uncertainty, converted_mags, converted_mags_errors, mag_diff, mag_diff_err
 
 
 def convert_panstars_i_dec_mags(panstars_cat: pd.DataFrame):
@@ -62,11 +55,8 @@ def convert_panstars_i_dec_mags(panstars_cat: pd.DataFrame):
     i_uncertainties = panstars_cat['iMeanPSFMagErr'].values
     r_uncertainties = panstars_cat['rMeanPSFMagErr'].values
 
-    i_decam = i_panstars_mags - 0.155 * \
-        (r_panstars_mags - i_panstars_mags) + 0.015
-
-    converted_i_uncertainties = np.hypot(
-        i_uncertainties, 0.155*np.hypot(i_uncertainties, r_uncertainties))
+    i_decam = i_panstars_mags - 0.155 * (r_panstars_mags - i_panstars_mags) + 0.015
+    converted_i_uncertainties = np.hypot(i_uncertainties, 0.155*np.hypot(i_uncertainties, r_uncertainties))
 
     return i_decam, converted_i_uncertainties
 
@@ -81,11 +71,8 @@ def convert_panstars_z_dec_mags(panstars_cat: pd.DataFrame):
     r_uncertainties = panstars_cat['rMeanPSFMagErr'].values
     z_uncertainties = panstars_cat['zMeanPSFMagErr'].values
 
-    z_decam = z_panstars_mags - 0.114 * \
-        (r_panstars_mags - i_panstars_mags) - 0.010
-
-    converted_z_uncertainties = np.hypot(
-        z_uncertainties, 0.114*np.hypot(i_uncertainties, r_uncertainties))
+    z_decam = z_panstars_mags - 0.114 * (r_panstars_mags - i_panstars_mags) - 0.010
+    converted_z_uncertainties = np.hypot(z_uncertainties, 0.114*np.hypot(i_uncertainties, r_uncertainties))
 
     return z_decam, converted_z_uncertainties
 
@@ -96,19 +83,16 @@ CONVERT = {
 }
 
 
-def calculate_difference_between_mags(
-        decam_mags, decam_mags_uncertainties, converted_decam_mags, converted_decam_mags_uncertainties):
+def calculate_difference_between_mags(decam_mags, decam_mags_uncertainties, converted_decam_mags, converted_decam_mags_uncertainties):
     """Working out the y_axis of the plot."""
     difference = decam_mags - converted_decam_mags
-    difference_errors = np.hypot(
-        decam_mags_uncertainties, converted_decam_mags_uncertainties)
+    difference_errors = np.hypot(decam_mags_uncertainties, converted_decam_mags_uncertainties)
     return difference, difference_errors
 
 
 def prepare_plotting_data(sextractor_file_name, panstars_file_name, band):
     """Gets all the necessary variables for plotting ready and removes outliers."""
-    decam_catalog, panstars_catalog = read_in_wide_band(
-        sextractor_file_name, panstars_file_name)
+    decam_catalog, panstars_catalog = read_in_wide_band(sextractor_file_name, panstars_file_name)
     mags = make_wide_band_mags(decam_catalog, panstars_catalog, band=band)
     msk = remove_outliers(mags[-2], sigma=2)
     cleaned_mags = [mag[msk] for mag in mags]
@@ -117,8 +101,7 @@ def prepare_plotting_data(sextractor_file_name, panstars_file_name, band):
 
 def plot_direct_comparison(mag_1, mag_1_errors, mag_2, mag_2_errors, x_label='', y_label=''):
     """Just plots the two magnitudes against one another"""
-    plt.errorbar(mag_1, mag_2, xerr=mag_1_errors,
-                 yerr=mag_2_errors, fmt='ko', alpha=0.5)
+    plt.errorbar(mag_1, mag_2, xerr=mag_1_errors, yerr=mag_2_errors, fmt='ko', alpha=0.5)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.show()
@@ -139,16 +122,14 @@ def plot_difference_fit(
         diff_cut = diff[cut]
         diff_err_cut = diff_err[cut]
 
-        a_fit, cov = curve_fit(
-            straight_line, mag_cut, diff_cut, sigma=diff_err_cut, absolute_sigma=True)
+        a_fit, cov = curve_fit(straight_line, mag_cut, diff_cut, sigma=diff_err_cut, absolute_sigma=True)
     else:
-        a_fit, cov = curve_fit(straight_line, mag, diff,
-                               sigma=diff_err, absolute_sigma=True)
+        a_fit, cov = curve_fit(straight_line, mag, diff, sigma=diff_err, absolute_sigma=True)
     
     uncertainties = np.sqrt(np.diag(cov))
-    NSTD = 5.
-    popt_up = a_fit + NSTD * uncertainties
-    popt_dw = a_fit - NSTD * uncertainties
+    nstd = 5.
+    popt_up = a_fit + nstd * uncertainties
+    popt_dw = a_fit - nstd * uncertainties
     x_fit = np.linspace(np.sort(mag)[0], np.sort(mag)[-1])
     fit = straight_line(x_fit, *a_fit)
     fit_up = straight_line(x_fit, *popt_up)
@@ -156,7 +137,6 @@ def plot_difference_fit(
 
     c = a_fit[0]
     plotting.start_plot(x_label = x_label, y_label=y_label)
-    #plt.title(f'{c} +- {uncertainties[0]}')
     print(f'HELLO THIS THE ZPT: {c} +- {uncertainties[0]}')
     plt.errorbar(mag, diff, xerr=mag_err, yerr=diff_err,
                  fmt='ko', ms=3, capsize=0, alpha=0.5)
@@ -168,16 +148,13 @@ def plot_difference_fit(
 
 def plot_depth(
     decam_file_name: str, zero_point: float, x_label: str, y_label: str,
-    outfile: str, exp_map: str) -> None:
+    outfile: str) -> None:
     """Makes a mag vs mag_err plot from all the available detections."""
     plt.show()
     decam_catalog = SExtractorCat(decam_file_name)
-    decam_catalog.remove_sources_based_on_exposure_map(exp_map=exp_map)
     decam_mag = decam_catalog.catalog['MAG_AUTO'].values
     mag = decam_mag + zero_point
     mag_err = decam_catalog.catalog['MAGERR_AUTO'].values
-    #idx = random.sample(range(len(mag)), 20000)
-    #x_values, y_values = mag[idx], mag_err[idx]
     cut_x = np.where(mag < 50)
     cut_y = np.where(mag_err < 1000)
     cut = np.intersect1d(cut_x, cut_y)
@@ -209,59 +186,25 @@ def plot_depth(
 
 
 if __name__ == '__main__':
-    PLOT_FOLDER = './plots/'
-    INFILE_SEX = '../correct_stacks/i/test.cat'
+    PLOT_FOLDER = 'plots/'
+    INFILE_SEX = '../correct_stacks/N964/i.cat'
     INFILE_PAN = '../PANSTARS/PANSTARS_i.csv'
-    EXP_MAP = '../correct_stacks/i/c4d_211021_003940_ose_i_vik1.fits.fz'
     mags = prepare_plotting_data(INFILE_SEX, INFILE_PAN, 'i')
 
-    plot_direct_comparison(
-        mags[0], mags[1],
-        mags[2], mags[3],
-        x_label='Decam Mags', y_label='Panstars_Mags')
+    #plot_direct_comparison(mags[0], mags[1],mags[2], mags[3], x_label='Decam Mags', y_label='Panstars_Mags')
+    #plot_direct_comparison(mags[0], mags[1], mags[4], mags[5], x_label='Decam Mags', y_label='Panstars Converted into Decam')
 
-    plot_direct_comparison(
-        mags[0], mags[1],
-        mags[4], mags[5],
-        x_label='Decam Mags', y_label='Panstars Converted into Decam')
+    plot_difference_fit(mags[0], mags[6], mags[1], mags[7], x_lim=(-15.208, -11.497), y_lim=(-31.6, -30.44),outfile = PLOT_FOLDER + 'i_zpt.png', x_label=r'$i_{\rm DECam}$', y_label = r'$i_{\rm converted ps1}$')
+    plot_depth(INFILE_SEX, 30.870, x_label='Decam i magnitudes', y_label='SNR', outfile='plots/depth_i.png')
 
-    plot_difference_fit(mags[0], mags[6], mags[1], mags[7],
-                        x_lim=(-15.208, -11.497), y_lim=(-31.6, -30.44),
-                        outfile = PLOT_FOLDER + 'i_zpt.png', x_label=r'$i_{\rm DECam}$',
-                        y_label = r'$i_{\rm converted ps1}$')
-
-
-    plot_depth(INFILE_SEX, 30.870, x_label='Decam i magnitudes',
-               y_label='SNR', outfile='plots/depth_i.png', exp_map = EXP_MAP)
-
-    INFILE_SEX = '../correct_stacks/z/test.cat'
+    INFILE_SEX = '../correct_stacks/N964/z.cat'
     INFILE_PAN = '../PANSTARS/PANSTARS_z.csv'
-    EXP_MAP = '../correct_stacks/z/c4d_210831_053503_ose_z_vik1.fits'
     mags = prepare_plotting_data(INFILE_SEX, INFILE_PAN,'z')
 
-    plot_direct_comparison(
-        mags[0], mags[1],
-        mags[2], mags[3],
-        x_label='Decam Mags', y_label='Panstars_Mags')
+    #plot_direct_comparison(mags[0], mags[1], mags[2], mags[3], x_label='Decam Mags', y_label='Panstars_Mags')
+    #plot_direct_comparison(mags[0], mags[1], mags[4], mags[5], x_label='Decam Mags', y_label='Panstars Converted into Decam')
+    plot_difference_fit(mags[0], mags[6], mags[1], mags[7],x_lim=(-15.9, -12.2), y_lim=(-30.611, -30.455),outfile=PLOT_FOLDER + 'z_zpt.png', x_label = r'$z_{\rm DECam}$', y_label = r'$z_{\rm converted ps1}$')
+    plot_depth(INFILE_SEX, 30.538, x_label='Decam z magnitudes', y_label='SNR', outfile='plots/depth_z.png')
 
-    plot_direct_comparison(
-        mags[0], mags[1],
-        mags[4], mags[5],
-        x_label='Decam Mags', y_label='Panstars Converted into Decam')
 
-    plot_difference_fit(mags[0], mags[6], mags[1], mags[7],
-                        x_lim=(-15.9, -12.2), y_lim=(-30.611, -30.455),
-                        outfile=PLOT_FOLDER + 'z_zpt.png',
-                        x_label = r'$z_{\rm DECam}$', y_label = r'$z_{\rm converted ps1}$')
-
-    plot_depth(INFILE_SEX, 30.538, x_label='Decam z magnitudes',
-                y_label='SNR', outfile='plots/depth_z.png', exp_map = EXP_MAP)
-
-    plot_depth(
-        '../correct_stacks/N964/test.cat',
-        29.01,
-        'N964 Magnitudes',
-        'SNR',
-        'plots/N964_Depth.png',
-        '../correct_stacks/N964/c4d_210831_050404_ose_N964_vik1.fits'
-    )
+    plot_depth('../correct_stacks/N964/n964.cat', 29.01,'N964 Magnitudes', 'SNR', 'plots/N964_Depth.png')
--- narrow_band.py
+++ narrow_band.py
@@ -12,25 +12,10 @@ def straight_line(parameters, x_val):
     alpha, zpt = parameters
     return x_val * alpha + zpt
 
-def fwhm_plot(sex_cat: SExtractorCat, psf_type: str = 'WORLD'):
-    """makes the fwhm showing the difference fwhm."""
-    plotting.start_plot('FWHM', 'Counts')
-    plt.hist(sex_cat.catalog[f'FWHM_{psf_type}'], bins = 1000, color='r', lw=3, histtype='step')
-    plt.hist(sex_cat.catalog[f'FWHMPSF_{psf_type}'], bins = 1000, color='b', lw=3, histtype='step')
-    plt.legend()
-    print(f'FWHMPSF {psf_type}: ', np.median(sex_cat.catalog[f'FWHMPSF_{psf_type}']))
-    print(f'FWHM: {psf_type}', np.median(sex_cat.catalog[f'FWHM_{psf_type}']))
-    plotting.end_plot('fwhm.png')
-
-def make_region_file(decam_file:str, sex_file: str) -> None:
-    """Makes the region file quickly."""
-    sex_cat = SExtractorCat(sex_file)
-    sex_cat.to_region_file(decam_file,'N964.reg')
-
 if __name__ == '__main__':
-    INFILE_SEX = '/media/trystan/TOSHIBA EXT/DECAM/correct_stacks/N964/test.cat'
-    INFILE_Z = '/home/trystan/Desktop/Work/PhD/PANSTARS/PANSTARS_z.csv'
-    INFILE_Y = '/home/trystan/Desktop/Work/PhD/PANSTARS/PANSTARS_y.csv'
+    INFILE_SEX = '../correct_stacks/N964/n964.cat'
+    INFILE_Z = '../PANSTARS/PANSTARS_z.csv'
+    INFILE_Y = '../PANSTARS/PANSTARS_y.csv'
 
     decam_all = SExtractorCat(INFILE_SEX)
     decam_catalog_z, pan_cat_z = decam_all.cross_match_with_panstars(INFILE_Z)
@@ -40,24 +25,22 @@ if __name__ == '__main__':
     y = pan_cat_z['yMeanPSFMag'].values
     z_err = pan_cat_z['zMeanPSFMagErr'].values
     y_err = pan_cat_z['zMeanPSFMagErr'].values
-    n964 = decam_catalog_z['MAG_BEST'].values
-    n964_err = decam_catalog_z['MAGERR_BEST'].values
+    n964 = decam_catalog_z['MAG_AUTO'].values
+    n964_err = decam_catalog_z['MAGERR_AUTO'].values
     cut = np.where(y != -999)[0]
-    z, y, n964, z_err, y_err, n964_err = z[cut], y[cut], n964[cut],\
-         z_err[cut], y_err[cut], n964_err[cut]
+    z, y, n964, z_err, y_err, n964_err = z[cut], y[cut], n964[cut], z_err[cut], y_err[cut], n964_err[cut]
 
     delta_pan = z - y
     delta_decam = z - n964
     delta_pan_err = np.hypot(z_err, y_err)
     delta_decam_err = np.hypot(z_err, n964_err)
     cut_x = np.where((delta_pan > -0.2) & (delta_pan < 0.6))[0]
-    cut_y = np.where((delta_decam > 28.679) & (delta_decam < 29.752))
-    final_cut = np.intersect1d(cut_x, cut_y)
+    cut_y = np.where((delta_decam > 28.75) & (delta_decam < 29.752))
+    final_cut = np.arange(len(delta_pan))#np.intersect1d(cut_x, cut_y)
 
     straight_line_model = odr.Model(straight_line)
-    data = odr.RealData(
-        delta_pan[final_cut], delta_decam[final_cut],
-        sx=delta_pan[final_cut], sy=delta_decam[final_cut])
+    data = odr.RealData(delta_pan[final_cut], delta_decam[final_cut], 
+                        sx=delta_pan[final_cut], sy=delta_decam[final_cut])
 
     _odr = odr.ODR(data, straight_line_model, beta0=[0.81, 28.9])
     out = _odr.run()
@@ -77,25 +60,18 @@ if __name__ == '__main__':
     fit_dw= straight_line(popt_dw, x_fit)
 
     fig = plotting.start_plot(x_label = 'z - y', y_label = 'N964 - z')
-    plt.errorbar(
-        delta_pan[final_cut],
-        delta_decam[final_cut],
-        xerr=delta_pan_err[final_cut],
-        yerr = delta_decam_err[final_cut],
-        fmt = 'ko', alpha=0.3, capsize=0, zorder=0)
+    plt.errorbar(delta_pan[final_cut], delta_decam[final_cut], xerr=delta_pan_err[final_cut],
+                  yerr = delta_decam_err[final_cut], fmt = 'ko', alpha=0.3, capsize=0, zorder=0)
 
     plt.plot(x_fit, fit,'r', lw=2)
     plt.fill_between(x_fit, fit_up, fit_dw, alpha=.5, color='r')
     plt.xlim(-0.3, 0.7)
-    plotting.end_plot('/home/trystan/Desktop/Work/PhD/main/plots/N964_zpt_calculation.png')
+    plotting.end_plot('plots/N964_zpt_calculation.png')
 
-    N964_DIR = '/media/trystan/TOSHIBA EXT/DECAM/correct_stacks/N964/'
-    N964_SEX_CAT = N964_DIR + 'test.cat'
+    N964_DIR = '../correct_stacks/N964/'
+    N964_SEX_CAT = N964_DIR + 'n964.cat'
     N964_DECAM_CAT = N964_DIR + 'c4d_210831_050404_osj_N964_vik1.fits.fz'
-    N964_EXP_MAP = N964_DIR + 'c4d_210831_050404_ose_N964_vik1.fits'
 
     n964_sex_cat = SExtractorCat(N964_SEX_CAT)
-    n964_sex_cat.remove_sources_based_on_exposure_map(N964_EXP_MAP)
-    n964_sex_cat.catalog = n964_sex_cat.catalog[n964_sex_cat.catalog['MAG_BEST'] < 3]
-    fwhm_plot(n964_sex_cat)
+    n964_sex_cat.catalog = n964_sex_cat.catalog[n964_sex_cat.catalog['MAG_AUTO'] < 3]
     n964_sex_cat.to_region_file(N964_DECAM_CAT, N964_DIR + 'N964.reg')
