d7eaf1c46b219a7000ff671d7c10de8cce504d78
Trystan Lambert <trystanscottlambert@gmail.com>
2023-03-22 15:56:15 -0300

    refactoring mask_negative_pixels

--- mask_negative_pixels.py
+++ mask_negative_pixels.py
@@ -1,32 +1,81 @@
-"""Masking the very negative pixels of the DECam image before running through pipeline."""
+"""
+This module provides functions to mask very negative pixel values in DECam FITS images.
+
+The module contains two functions:
+
+    - mask_very_negative_pixels: Replace very negative pixel values with NaNs in a 2D NumPy array.
+    - mask_fits_file: Mask the very negative pixel values in a DECam FITS file by calling
+      mask_very_negative_pixels on each HDU data array.
+
+Usage:
+------
+To mask the very negative pixel values in a set of DECam FITS files,
+provide a list of filepaths to mask_fits_file.
+
+Example:
+--------
+Suppose you have a set of DECam FITS files:
+
+    - image1.fits
+    - image2.fits
+    - image3.fits
+
+To mask the very negative pixel values in each file, call mask_fits_file with a list of filepaths:
+
+    from mask_neg_pixels import mask_fits_file
+
+    input_files = ['image1.fits', 'image2.fits', 'image3.fits']
+    for file_path in input_files:
+        mask_fits_file(file_path)
+
+This will create new files with the same names as the input files,
+with the very negative pixel values masked with NaNs.
+"""
 
 import numpy as np
 from astropy.io import fits
 
+def mask_very_negative_pixels(data: np.ndarray) -> np.ndarray:
+    """
+    Replace very negative pixel values with NaNs.
 
-def convert_neg_to_nan(array_2d):
-    """Returns a 2d array with the very negative values masked with nans."""
-    neg_idx = np.where(array_2d < 0)
-    array_2d[neg_idx] = 400
-    return array_2d
+    Parameters
+    ----------
+    data : numpy.ndarray
+        2D array of pixel values.
 
-def mask_neg_values(hdu_list):
-    """Loops through the entire hdu list and masks the very negative values."""
-    for i in range(1, len(hdu_list)): # 0th value is the headers
-        hdu_list[i].data = convert_neg_to_nan(hdu_list[i].data)
-    return hdu_list
+    Returns
+    -------
+    numpy.ndarray
+        2D array with very negative values masked with NaNs.
+    """
+    masked_data = np.where(data < 0, np.nan, data)
+    return masked_data
 
-def mask_fits_file(fits_name: str):
-    """masks the very negative values of a given fits file."""
-    hdu = fits.open(fits_name)
-    hdu = mask_neg_values(hdu)
-    hdu.writeto(fits_name, overwrite=True)
+def mask_fits_file(filepath: str) -> None:
+    """
+    Mask the very negative pixel values in a FITS file.
+
+    Parameters
+    ----------
+    filepath : str
+        Filepath of the FITS file to mask.
+
+    Returns
+    -------
+    None
+    """
+    with fits.open(filepath) as hdulist:
+        for hdu in hdulist[1:]:
+            hdu.data = mask_very_negative_pixels(hdu.data)
+        hdulist.writeto(filepath, overwrite=True)
 
 if __name__ == '__main__':
-    INFILE_N964 = '../correct_stacks/N964/c4d_210831_050404_osj_N964_vik1.fits'
-    INFILE_I = '../correct_stacks/i/c4d_211021_003940_osj_i_vik1.fits'
-    INFILE_Z = '../correct_stacks/z/c4d_210831_053503_osj_z_vik1.fits'
-    infiles = [INFILE_N964, INFILE_I, INFILE_Z]
+    input_files = [
+        '../correct_stacks/N964/c4d_210831_050404_osj_N964_vik1.fits',
+        '../correct_stacks/i/c4d_211021_003940_osj_i_vik1.fits',
+        '../correct_stacks/z/c4d_210831_053503_osj_z_vik1.fits'
+    ]
 
-    for file in infiles:
-        mask_fits_file(file)
+    for file_path in input_files:
+        mask_fits_file(file_path)
