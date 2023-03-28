49a03f4d18e2de4474fb6cb2d15d5f58c99a72fe
Trystan Lambert <trystanscottlambert@gmail.com>
2023-03-28 15:18:34 -0300

    Added DECAM outline to the onsky dist plot.

--- plot_onsky_distribution.py
+++ plot_onsky_distribution.py
@@ -1,13 +1,14 @@
 """Plotting the onsky distribution"""
 
 import numpy as np
-from matplotlib import pyplot as plt, patches
+from matplotlib import pyplot as plt
+from regions import PixCoord, CirclePixelRegion
 import astropy.units as u
 from astropy.cosmology import FlatLambdaCDM
 from astropy.io import fits
 from astropy.wcs import WCS
+from regionfy_catalog import load_region
 import plotting
-from regions import PixCoord, CirclePixelRegion
 
 
 def get_radius(physical_radius_mpc: float) -> float:
@@ -16,6 +17,7 @@ def get_radius(physical_radius_mpc: float) -> float:
     deg_per_mpc = arcsec_per_kpc.to(u.deg / u.Mpc)
     physical_radius_deg = physical_radius_mpc * u.Mpc * deg_per_mpc
     return physical_radius_deg.value
+
 
 DECAM_SHAPE_FILE = '../correct_stacks/N964/n964_weight.fits'
 decam_hdu = fits.open(DECAM_SHAPE_FILE)
@@ -26,6 +28,8 @@ DEC_QSO = (30 + (54/60) + (10.0/3600)) * -1
 REDSHIFT_QSO = 6.9
 COSMO = FlatLambdaCDM(H0=70, Om0=0.3)
 DEG_PER_PIX = np.abs(decam_hdu[0].header['PC2_2'])
+REGION_FILE = 'DECAM.reg'
+region_decam_fov = load_region(REGION_FILE)
 
 INFILE = 'candidates.txt'
 ra, dec = np.loadtxt(INFILE, unpack=True)
@@ -36,13 +40,16 @@ region_1 = CirclePixelRegion(center=PixCoord(x=ra_qso_plot, y=dec_qso_plot), rad
 region_10 = CirclePixelRegion(center=PixCoord(x=ra_qso_plot, y=dec_qso_plot), radius=get_radius(10) / DEG_PER_PIX)
 region_20 = CirclePixelRegion(center=PixCoord(x=ra_qso_plot, y=dec_qso_plot), radius=get_radius(20) / DEG_PER_PIX)
 
-fig = plt.figure()
+fig = plt.figure(figsize = (3.54, 3.54), dpi = 600)
 ax = fig.add_subplot(projection = decam_wcs)
+ax.set_xlabel('RA')
+ax.set_ylabel('DEC')
 ax.imshow(decam_hdu[0].data, alpha=0)
 region_1.plot(ax=ax, color='red', lw=2.0, ls=':')
 region_10.plot(ax=ax, color='red', lw=2.0, ls=':')
 region_20.plot(ax=ax, color='red', lw=2.0, ls=':')
+region_decam_fov.plot(ax = ax, color='k', lw=2.0)
 ax.scatter(ra_plot, dec_plot)
 ax.scatter(ra_qso_plot, dec_qso_plot, marker='*', s=100, color='k')
-
-plt.show()
+#ax.set_axis_off()
+plotting.end_plot('plots/on_sky_distribution.png')
