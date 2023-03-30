04544957b7c88e9f20635b692c337934c74ee2be
Trystan Lambert <trystanscottlambert@gmail.com>
2023-03-30 09:41:31 -0300

    remove cosmic rays running

--- remove_cosmic_rays.py
+++ remove_cosmic_rays.py
@@ -1,11 +1,12 @@
 """Trying to remove the cosmic rays from the images."""
 
 from astropy.io import fits
-import lacosmic
+import astroscrappy as asc
 
 if __name__ == '__main__':
-    INFILE = '../correct_stacks/N964/n964.fits'
-    hdu = fits.open(INFILE)
-    msk, array = lacosmic.lacosmic(hdu[0].data)
-    hdu[0].data = array
-    hdu.writeto('n964_remove_cosmics.fits')
+    INFILE = '../correct_stacks/N964/c4d_210831_050404_osj_N964_vik1.fits'
+    hdul = fits.open(INFILE)
+    for hdu in hdul[1:]:
+        msk, array = asc.detect_cosmics(hdu.data)
+        hdu.data = array
+    hdul.writeto('n964_remove_cosmics.fits')
