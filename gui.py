0753c3f3ca19e62c6bd212e578bc09bd8de1062d
Trystan Lambert <trystanscottlambert@gmail.com>
2023-05-30 09:29:43 -0400

    Adding z scale to gui

--- gui.py
+++ gui.py
@@ -5,13 +5,21 @@ from matplotlib.backends.backend_tkagg import (
 # Implement the default Matplotlib key bindings.
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
-
 import numpy as np
+from astropy.visualization import ZScaleInterval
 
 COUNTER = 0
 POSSIBLE = []
 ARTIFACT = []
 
+
+def get_limits(array_2d: np.ndarray) -> tuple[float, float]:
+    """
+    Returns the Zscale limits for the given data.
+    """
+    zscale = ZScaleInterval()
+    v_min, v_max = zscale.get_limits(array_2d)
+    return v_min, v_max
 
 def start_gui(i_bands, z_bands, n_bands):
 
@@ -19,21 +27,27 @@ def start_gui(i_bands, z_bands, n_bands):
     root.wm_title("Finding Artifacts")
 
     fig = Figure(figsize=(5, 4), dpi=100)
-    t = np.arange(0, 3, .01)
     ax_i = fig.add_subplot(131)
-    i_band_display = ax_i.imshow(i_bands[COUNTER])
+    i_data = i_bands[COUNTER]
+    i_limits = get_limits(i_data)
+
+    i_band_display = ax_i.imshow(i_data, vmin = i_limits[0], vmax = i_limits[1])
     ax_i.set_xlabel('i-band')
     ax_i.set_xticks([])
     ax_i.set_yticks([])
 
     ax_z = fig.add_subplot(132)
-    z_band_display = ax_z.imshow(z_bands[COUNTER])
+    z_data = z_bands[COUNTER]
+    z_limits = get_limits(z_data)
+    z_band_display = ax_z.imshow(z_data, vmin=z_limits[0], vmax=z_limits[1])
     ax_z.set_xlabel('z-band')
     ax_z.set_xticks([])
     ax_z.set_yticks([])
 
     ax_n = fig.add_subplot(133)
-    n_band_display = ax_n.imshow(n_bands[COUNTER])
+    n_data = n_bands[COUNTER]
+    n_limits = get_limits(n_data)
+    n_band_display = ax_n.imshow(n_data, vmin=n_limits[0], vmax=n_limits[1])
     ax_n.set_xlabel('n-band')
     ax_n.set_xticks([])
     ax_n.set_yticks([])
