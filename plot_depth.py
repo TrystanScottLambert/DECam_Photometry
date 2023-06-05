7486ae7cd2e76f1905b290b98a86626437a83366
Trystan Lambert <trystanscottlambert@gmail.com>
2023-06-05 15:33:15 -0400

    Checking depth plots

--- plot_depth.py
+++ plot_depth.py
@@ -6,6 +6,7 @@ from scipy.stats import gaussian_kde
 import plotting
 from sex_catalog import SExtractorCat
 from zero_points import zero_points
+from zero_points_cdfs import zero_points_cdfs
 
 
 HU_VALUES = {
@@ -41,15 +42,15 @@ def plot_depth(sextractor_cat_name: str, zpt: float, broadband: str) -> None:
     mag, signal_to_noise  = mag[signal_to_noise_cut], signal_to_noise[signal_to_noise_cut]
 
     # Do some shadding instead of a scatter plot.
-    #nbins = 100
-    #kernal = gaussian_kde((mag, signal_to_noise))
-    #x_i, y_i = np.mgrid[
-    #    mag.min():mag.max():nbins*1j, signal_to_noise.min():signal_to_noise.max():nbins*1j]
-    #z_i = kernal(np.vstack([x_i.flatten(), y_i.flatten()]))
+    nbins = 100
+    kernal = gaussian_kde((mag, signal_to_noise))
+    x_i, y_i = np.mgrid[
+        mag.min():mag.max():nbins*1j, signal_to_noise.min():signal_to_noise.max():nbins*1j]
+    z_i = kernal(np.vstack([x_i.flatten(), y_i.flatten()]))
     plotting.start_plot(x_label='Measured Mags', y_label='SNR')
-    #plt.pcolormesh(x_i, y_i, z_i.reshape(x_i.shape), shading='auto')
-    plt.scatter(mag, signal_to_noise, s= 1, color='k', alpha=0.3)
-    plt.axhline(2, ls='--', lw=1.5, color='r', alpha=0.4)
+    plt.pcolormesh(x_i, y_i, z_i.reshape(x_i.shape), shading='auto')
+    #plt.scatter(mag, signal_to_noise, s= 1, color='k', alpha=0.3)
+    plt.axhline(5, ls='--', lw=1.5, color='r', alpha=0.4)
     plt.axvline(HU_VALUES[broadband], ls = ':', color='k')
     plt.xlim(22, 28)
     plotting.end_plot(f'plots/depth_{broadband}.png')
@@ -65,7 +66,22 @@ if __name__ == '__main__':
     N_ZPT = zero_points.n964_band.mag_correct(1)
     N_ZPT_135 = zero_points.n964_band.mag_correct(1.35/2)
 
+
+    INFILE_SEX_I_CDFS = '../CDFS_LAGER/i_cdfs.cat'
+    INFILE_SEX_Z_CDFS = '../CDFS_LAGER/z_cdfs.cat'
+    INFILE_SEX_N_CDFS = '../CDFS_LAGER/n964_cdfs.cat'
+    I_ZPT_CDFS = zero_points_cdfs.i_band.mag_correct(1)
+    Z_ZPT_CDFS = zero_points_cdfs.z_band.mag_correct(1)
+    N_ZPT_CDFS = zero_points_cdfs.n964_band.mag_correct(1)
+    N_ZPT_135_CDFS = zero_points_cdfs.n964_band.mag_correct(1.35/2)
+
+
+
     plot_depth(INFILE_SEX_I, I_ZPT, 'i')
     plot_depth(INFILE_SEX_Z, Z_ZPT, 'z')
     plot_depth(INFILE_SEX_N, N_ZPT, 'n964')
     #plot_depth(INFILE_SEX_N_135, N_ZPT_135, 'n964')
+
+    plot_depth(INFILE_SEX_I_CDFS, I_ZPT_CDFS, 'i')
+    plot_depth(INFILE_SEX_Z_CDFS, Z_ZPT_CDFS, 'z')
+    plot_depth(INFILE_SEX_N_CDFS, N_ZPT_CDFS, 'n964')
