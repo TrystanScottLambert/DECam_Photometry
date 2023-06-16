1bc4dd9bb4599de72cf4f19bc78d9fde2ee7b3fe
Trystan Lambert <trystanscottlambert@gmail.com>
2023-06-16 15:16:40 -0400

    Updating candidate search.

--- identify_candidates_v2.py
+++ identify_candidates_v2.py
@@ -337,8 +337,8 @@ if __name__ == '__main__':
 
     #our_selection = MagCutSelection(our_inputs, n_depth, n_135_depth, i_depth, z_depth)
     #our_selection_classic = ClassicSNR(our_inputs, 5, 5, 3, 3)
-    #cdfs_selection = MagCutSelection(cdfs_inputs, n_depth, n_135_depth, i_depth, z_depth)
-    cdfs_selection_classic = ClassicSNR(cdfs_inputs, 5, 5, 3, 3)
+    cdfs_selection = MagCutSelection(cdfs_inputs, n_depth, n_135_depth, i_depth, z_depth)
+    #cdfs_selection_classic = ClassicSNR(cdfs_inputs, 5, 5, 3, 3)
 
     #perform_selection(our_selection)
-    perform_selection(cdfs_selection_classic)
+    perform_selection(cdfs_selection)
