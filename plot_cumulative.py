ffb9444b3dec8f4bff29510d629dc00052d22b85
TrystanScottLambert <TrystanScottLambert@gmail.com>
2023-06-27 00:12:03 +0200

    Adding cummulative plot

--- plot_cumulative.py
+++ plot_cumulative.py
@@ -14,47 +14,62 @@ from zero_points_cdfs import zero_points_cdfs
 from plotting import start_plot, end_plot
 
 
-def cross_match_to_sexcat(ra_array: np.ndarray, dec_array:np.ndarray, sex_catalog: SExtractorCat) -> DataFrame:
+def cross_match_to_sexcat(
+        ra_array: np.ndarray, dec_array:np.ndarray, sex_catalog: SExtractorCat
+        ) -> DataFrame:
     """Reduces the sexcatalog to the cross matched values"""
-    sex_ra, sex_dec = np.array(sex_catalog.catalog['ALPHAPEAK_J2000']), np.array(sex_catalog.catalog['DELTAPEAK_J2000'])
+    sex_ra, sex_dec = np.array(
+        sex_catalog.catalog['ALPHAPEAK_J2000']), np.array(sex_catalog.catalog['DELTAPEAK_J2000'])
     catalog = SkyCoord(ra = sex_ra*u.deg, dec = sex_dec*u.deg)
     candidate_coords = SkyCoord(ra = ra_array * u.deg, dec = dec_array*u.deg)
     idx, _, _ = candidate_coords.match_to_catalog_sky(catalog)
     return sex_catalog.catalog.iloc[idx]
 
 
-i_cat = SExtractorCat('../correct_stacks/N964/i.cat')
-z_cat = SExtractorCat('../correct_stacks/N964/z.cat')
-n_cat = SExtractorCat('../correct_stacks/N964/n964.cat')
-n135_cat = SExtractorCat('../correct_stacks/N964/n964_135.cat')
+#n_cat = SExtractorCat('../correct_stacks/N964/n964.cat')
+#n135_cat = SExtractorCat('../correct_stacks/N964/n964_135.cat')
+n_cat = SExtractorCat('imacs_n964.cat')
+n135_cat = SExtractorCat('imacs_n964_135.cat')
 
-i_cdfs_cat = SExtractorCat('../CDFS_LAGER/i_cdfs.cat')
-z_cdfs_cat = SExtractorCat('../CDFS_LAGER/z_cdfs.cat')
 n_cdfs_cat = SExtractorCat('../CDFS_LAGER/n964_cdfs.cat')
 n135_cdfs_cat = SExtractorCat('../CDFS_LAGER/n964_135_cdfs.cat')
 
-candidates = 'candidates.txt'
+candidates = 'candidates_imacs.txt'
+#candidates = 'candidates.txt'
 candidates_cdfs = 'candidates_cdfs.txt'
 
 ra_us, dec_us = np.loadtxt(candidates, unpack=True)
 ra_cdfs, dec_cdfs = np.loadtxt(candidates_cdfs, unpack=True)
 
-matched_i_cat = cross_match_to_sexcat(ra_us, dec_us, i_cat)
-matched_z_cat = cross_match_to_sexcat(ra_us, dec_us, z_cat)
 matched_n_cat = cross_match_to_sexcat(ra_us, dec_us, n_cat)
 matched_n135_cat = cross_match_to_sexcat(ra_us, dec_us, n135_cat)
-
-matched_i_cat_cdfs = cross_match_to_sexcat(ra_cdfs, dec_cdfs, i_cdfs_cat)
-matched_z_cat_cdfs = cross_match_to_sexcat(ra_cdfs, dec_cdfs, z_cdfs_cat)
 matched_n_cat_cdfs = cross_match_to_sexcat(ra_cdfs, dec_cdfs, n_cdfs_cat)
 matched_n135_cat_cdfs = cross_match_to_sexcat(ra_cdfs, dec_cdfs, n135_cdfs_cat)
 
-### Manual method:
-ratio = 2.87e7/20385305
-x = -np.arange(1, -0.1, -0.1)
-us = np.array([18, 22, 25, 29, 38, 56, 82, 121, 186, 251, 310])
-them = np.array([16, 17, 20, 22, 28, 35, 55, 82, 137, 222, 309])
-plt.plot(x, us, label='this work', lw=3, color='r')
-plt.plot(x, them * ratio, label='Hu', lw=2, ls=':', color='k')
+### Narrowband
+#ratio = 2.87e7/20385305
+ratio = 5.01046e6/20385305
+matched_n_cat['MAG_CORR'] = matched_n_cat['MAG_APER'] + zero_points.n964_band.mag_correct(1)
+matched_n135_cat['MAG_CORR'] = matched_n135_cat['MAG_APER'] + zero_points.n964_band.mag_correct(1.35/2)
+n135_lim = 25.10
+n_lim = 24.66
+depth = -np.arange(1, -0.01, -0.01)
+counts = []
+for i in depth:
+    cut_n = np.where(matched_n_cat['MAG_CORR'] < n_lim + i)[0]
+    cut_135 = np.where(matched_n135_cat['MAG_CORR'] < n135_lim + i)[0]
+    counts.append(len(np.intersect1d(cut_n, cut_135)))
+
+matched_n_cat_cdfs['MAG_CORR'] = matched_n_cat_cdfs['MAG_APER'] + zero_points_cdfs.n964_band.mag_correct(1)
+matched_n135_cat_cdfs['MAG_CORR'] = matched_n135_cat_cdfs['MAG_APER'] + zero_points_cdfs.n964_band.mag_correct(1.35/2)
+counts_cdfs = []
+for i in depth:
+    cut_n = np.where(matched_n_cat_cdfs['MAG_CORR'] < n_lim + i)[0]
+    cut_135 = np.where(matched_n135_cat_cdfs['MAG_CORR'] < n135_lim + i)[0]
+    counts_cdfs.append(len(np.intersect1d(cut_n, cut_135)))
+
+start_plot('Relative Narrow Band Selection', r'Counts per Area [arcsecond$^{-2}$]')
+plt.plot(depth, np.array(counts)/5.01046e6, label='This work', color='k', lw=3)
+plt.plot(depth, np.array(counts_cdfs)/20385305, label='CDFS', color='r', ls=':', lw=2)
 plt.legend()
-plt.show()
+end_plot('cumulative_plot.png')
